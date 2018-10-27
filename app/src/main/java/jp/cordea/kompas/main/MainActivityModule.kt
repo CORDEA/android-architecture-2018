6c2f501570da6399d28f81cf13b24b1c7fa5231b
Yoshihiro Tanaka <contact@cordea.jp>
2018-10-27 15:44:11 +0900

    bind presenter, view

--- app/src/main/java/jp/cordea/kompas/main/MainActivityModule.kt
+++ app/src/main/java/jp/cordea/kompas/main/MainActivityModule.kt
@@ -1,10 +1,22 @@
 package jp.cordea.kompas.main
 
+import dagger.Binds
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
 interface MainActivityModule {
-    @ContributesAndroidInjector
+    @ContributesAndroidInjector(modules = [
+        MainActivityBindModule::class
+    ])
     fun contributeMainActivity(): MainActivity
+}
+
+@Module
+interface MainActivityBindModule {
+    @Binds
+    fun bindPresenter(presenter: MainPresenter): MainContract.Presenter
+
+    @Binds
+    fun bindView(activity: MainActivity): MainContract.View
 }
