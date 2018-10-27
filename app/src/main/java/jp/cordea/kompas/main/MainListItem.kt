c7750a26c594a32b033a24815f3e30292ffe20a5
Yoshihiro Tanaka <contact@cordea.jp>
2018-10-27 15:13:25 +0900

    add list item

---
+++ app/src/main/java/jp/cordea/kompas/main/MainListItem.kt
@@ -0,0 +1,32 @@
+package jp.cordea.kompas.main
+
+import com.xwray.groupie.databinding.BindableItem
+import jp.cordea.kompas.R
+import jp.cordea.kompas.databinding.ListItemMainBinding
+import jp.cordea.kompas.infra.events.EventResponse
+import javax.inject.Inject
+
+class MainListItemViewModel(
+        val title: String,
+        val description: String
+) {
+    companion object {
+        fun from(eventResponse: EventResponse) =
+                MainListItemViewModel(
+                        eventResponse.title,
+                        eventResponse.description
+                )
+    }
+}
+
+class MainListItem @Inject constructor() : BindableItem<ListItemMainBinding>() {
+    private lateinit var model: MainListItemViewModel
+
+    fun update(model: MainListItemViewModel) = apply { this.model = model }
+
+    override fun getLayout(): Int = R.layout.list_item_main
+
+    override fun bind(binding: ListItemMainBinding, position: Int) {
+        binding.vm = model
+    }
+}
