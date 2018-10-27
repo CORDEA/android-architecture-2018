6dfd1dcc19c5bfe50196b5a413f981fdb791d857
Yoshihiro Tanaka <contact@cordea.jp>
2018-10-27 17:44:01 +0900

    implement activity

--- app/src/main/java/jp/cordea/kompas/detail/DetailActivity.kt
+++ app/src/main/java/jp/cordea/kompas/detail/DetailActivity.kt
@@ -1,19 +1,43 @@
 package jp.cordea.kompas.detail
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_detail.*
+import androidx.databinding.DataBindingUtil
+import dagger.android.AndroidInjection
+import jp.cordea.kompas.R
+import jp.cordea.kompas.databinding.ActivityDetailBinding
+import jp.cordea.kompas.main.MainListItemViewModel
+import javax.inject.Inject
 
 class DetailActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var adapter: DetailAdapter
+
+    private lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
-        setSupportActionBar(toolbar)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_detail)
+        setSupportActionBar(binding.toolbar)
+
+        binding.recyclerView.adapter = adapter
+        val model = intent.getParcelableExtra<MainListItemViewModel>(MODEL_KEY)
+        supportActionBar!!.title = model.title
+
+        adapter.updateDescription(DescriptionListItemViewModel.from(model))
+        adapter.updateInfo(InfoListItemViewModel.from(model))
+    }
+
+    companion object {
+        private const val MODEL_KEY = "MODEL_KEY"
+
+        fun newIntent(context: Context, model: MainListItemViewModel) =
+                Intent(context, DetailActivity::class.java).apply {
+                    putExtra(MODEL_KEY, model)
+                }
     }
 }
