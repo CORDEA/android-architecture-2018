3dbdbc66fb0e3456cc23452b7a19600504f89ad5
Yoshihiro Tanaka <contact@cordea.jp>
2018-10-27 19:10:31 +0900

    provides database, dao

--- app/src/main/java/jp/cordea/kompas/AppModule.kt
+++ app/src/main/java/jp/cordea/kompas/AppModule.kt
@@ -2,9 +2,12 @@ package jp.cordea.kompas
 
 import android.app.Application
 import android.content.Context
+import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import jp.cordea.kompas.infra.ConnpassApiUrlProvider
+import jp.cordea.kompas.infra.favorite.FavoriteDao
+import jp.cordea.kompas.infra.favorite.FavoriteDaoProvider
 
 @Module
 class AppModule {
@@ -12,7 +15,16 @@ class AppModule {
     fun provideContext(application: Application): Context = application.applicationContext
 
     @Provides
+    fun provideAppDatabase(context: Context) =
+            Room.databaseBuilder(context, AppDatabase::class.java, "kompas").build()
+
+    @Provides
     fun provideConnpassApiUrlProvider() = object : ConnpassApiUrlProvider {
         override val url: String get() = BuildConfig.API_BASE_URL
+    }
+
+    @Provides
+    fun provideFavoriteDaoProvider(appDatabase: AppDatabase) = object : FavoriteDaoProvider {
+        override val favoriteDao: FavoriteDao get() = appDatabase.favoriteDao()
     }
 }
