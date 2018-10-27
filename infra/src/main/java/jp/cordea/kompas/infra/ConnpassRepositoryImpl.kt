734c7de3ccb58c1bf82fcd5ec2b8421533ab802b
Yoshihiro Tanaka <contact@cordea.jp>
2018-10-27 13:54:03 +0900

    add repository implementation

---
+++ infra/src/main/java/jp/cordea/kompas/infra/ConnpassRepositoryImpl.kt
@@ -0,0 +1,15 @@
+package jp.cordea.kompas.infra
+
+import io.reactivex.Single
+import jp.cordea.kompas.infra.events.EventsResponse
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+internal class ConnpassRepositoryImpl @Inject constructor(
+        private val remoteDataSource: ConnpassRemoteDataSource,
+        private val localDataSource: ConnpassLocalDataSource
+) : ConnpassRepository {
+    override fun getEvents(keyword: String): Single<EventsResponse> =
+            localDataSource.getEvents().switchIfEmpty(remoteDataSource.getEvents(keyword))
+}
