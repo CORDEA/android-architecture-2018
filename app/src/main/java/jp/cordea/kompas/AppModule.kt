f1a301a9fdfb5473bfc24293945245591a4c38cb
Yoshihiro Tanaka <contact@cordea.jp>
2018-10-27 13:24:44 +0900

    provides url

--- app/src/main/java/jp/cordea/kompas/AppModule.kt
+++ app/src/main/java/jp/cordea/kompas/AppModule.kt
@@ -4,9 +4,15 @@ import android.app.Application
 import android.content.Context
 import dagger.Module
 import dagger.Provides
+import jp.cordea.kompas.infra.ConnpassApiUrlProvider
 
 @Module
 class AppModule {
     @Provides
     fun provideContext(application: Application): Context = application.applicationContext
+
+    @Provides
+    fun provideConnpassApiUrlProvider() = object : ConnpassApiUrlProvider {
+        override val url: String get() = BuildConfig.API_BASE_URL
+    }
 }
